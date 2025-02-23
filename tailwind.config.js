4f199111e9963b69d641ff4a0ecd94355d65331d
naram <naramalkoht@gmail.com>
2025-02-23 08:53:28 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        "./resources/**/*.blade.php",
+        "./resources/**/**/*.blade.php",
+        "./resources/**/*.js",
+        "./app/Filament/**/*.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
