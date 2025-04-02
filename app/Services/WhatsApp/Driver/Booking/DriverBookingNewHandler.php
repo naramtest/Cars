cb945546b183334e53059e4077a718d9053daae9
naram <naramalkoht@gmail.com>
2025-04-02 10:54:50 +0300

    add whatsapp url redirect

--- app/Services/WhatsApp/Driver/Booking/DriverBookingNewHandler.php
+++ app/Services/WhatsApp/Driver/Booking/DriverBookingNewHandler.php
@@ -15,8 +15,8 @@ class DriverBookingNewHandler extends AbstractNotificationHandler
         $vehicle = $modelData->vehicle;
 
         return $this->formatBodyParameters([
-            $driver->name, // 1 - Driver's name
-            $modelData->reference_number, // 2 - Booking ref
+            $driver->name ?? "Naram", // 1 - Driver's name
+            "1", // 2 - Booking ref
             $modelData->client_name, // 3 - Customer name
             $modelData->client_phone, // 4 - Customer phone
             $vehicle->model, // 5 - Vehicle model
@@ -25,9 +25,23 @@ class DriverBookingNewHandler extends AbstractNotificationHandler
             // 7 - Pickup date
             $modelData->end_datetime->format("H:i"), // 8 - Pickup time
             $modelData->pickup_address, // 9 - Pickup location
-            $modelData->destination_address, // 10 - Dropoff location
-            $modelData->notes ?? "", // 11 - Optional instructions
+            "home", // 10 - Dropoff location
+            $modelData->notes ?? "nothing", // 11 - Optional instructions
         ]);
+        //        [
+        //            $driver->name ?? "", // 1 - Driver's name
+        //            $modelData->reference_number ?? "1", // 2 - Booking ref
+        //            $modelData->client_name, // 3 - Customer name
+        //            $modelData->client_phone, // 4 - Customer phone
+        //            $vehicle->model, // 5 - Vehicle model
+        //            $vehicle->license_plate, // 6 - Vehicle plate
+        //            $modelData->start_datetime->format("Y-m-d"),
+        //            // 7 - Pickup date
+        //            $modelData->end_datetime->format("H:i"), // 8 - Pickup time
+        //            $modelData->pickup_address, // 9 - Pickup location
+        //            $modelData->destination_address, // 10 - Dropoff location
+        //            $modelData->notes ?? "", // 11 - Optional instructions
+        //        ]
     }
 
     public function prepareButtonData($modelData): array
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use App\Http\Controllers\WhatsAppWebhookController;
 use App\Models\Booking;
 use App\Services\WhatsApp\Driver\Booking\DriverBookingNewHandler;
 use App\Services\WhatsApp\WhatsAppNotificationService;
+use App\Settings\InfoSettings;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/webhook", [WhatsAppWebhookController::class, "verify"]);
 Route::post("/webhook", [WhatsAppWebhookController::class, "handleWebhook"]);
-
+Route::get("/whatsapp/contact", function (InfoSettings $infoSettings) {
+    return redirect()->away(
+        "https://wa.me/$infoSettings->support_whatsapp_number"
+    );
+});
 Route::get("/", function (WhatsAppNotificationService $whatsAppService) {
     $result = $whatsAppService->send(
         DriverBookingNewHandler::class,
